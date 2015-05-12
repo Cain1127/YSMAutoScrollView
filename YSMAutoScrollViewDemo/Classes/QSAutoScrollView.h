f395b245e6f10a1869c54551990fad73fe72d0bf
ysmeng <49427823@163.com>
2015-05-12 17:54:49 +0800

    添加使用demo-ysmeng

---
+++ YSMAutoScrollViewDemo/Classes/QSAutoScrollView.h
@@ -0,0 +1,128 @@
+//
+//  QSAutoScrollView.h
+//  House
+//
+//  Created by ysmeng on 15/1/19.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  @brief  自定义类广告自滚动view的协议，主要是方便添加滚动相关的数据源信息
+ */
+@class QSAutoScrollView;
+@protocol QSAutoScrollViewDelegate <NSObject>
+
+@required
+
+///自滚动的总数
+- (int)numberOfScrollPage:(QSAutoScrollView *)autoScrollView;
+
+/**
+ *  @author                 yangshengmeng, 15-01-19 11:01:27
+ *
+ *  @brief                  获取需要显示信息的view，同时需要传入一个单击时回调的参数
+ *
+ *  @param autoScrollView   自滚动view
+ *  @param index            显示view
+ *  @param params           单击时回调的参数
+ *
+ *  @return                 返回显示view
+ *
+ *  @since                  1.0.0
+ */
+- (UIView *)autoScrollViewShowView:(QSAutoScrollView *)autoScrollView
+                viewForShowAtIndex:(int)index;
+
+/**
+ *  @author yangshengmeng, 15-01-19 11:01:46
+ *
+ *  @brief  返回单击滚动栏时的回调参数
+ *
+ *  @return 返回单击时的参数
+ *
+ *  @since  1.0.0
+ */
+- (id)autoScrollViewTapCallBackParams:(QSAutoScrollView *)autoScrollView
+                   viewForShowAtIndex:(int)index;
+
+@end
+
+typedef enum
+{
+
+    aAutoScrollDirectionTypeTopToBottom = 200,  //!<从上往下滚动
+    aAutoScrollDirectionTypeBottomToTop,        //!<从下往上滚动
+    aAutoScrollDirectionTypeLeftToRight,        //!<从左到右滚动
+    aAutoScrollDirectionTypeRightToLeft         //!<从右到左滚动
+
+}AUTOSCROLL_DIRECTION_TYPE;                     //!<自滚动view的滚动方向类型
+
+/**
+ *  @author yangshengmeng, 15-01-19 10:01:02
+ *
+ *  @brief  类广告自滚动的自定义view
+ *
+ *  @since  1.0.0
+ */
+@interface QSAutoScrollView : UIView
+
+/**
+ *  @author                 yangshengmeng, 15-01-19 11:01:16
+ *
+ *  @brief                  根据代理和滚动类型，创建一个类广告自滚动的视图
+ *
+ *  @param frame            在父视图中的位置和大小
+ *  @param delegate         滚动视图的代理
+ *  @param directionType    滚动的方向
+ *
+ *  @return                 返回一个自滚动的视图
+ *
+ *  @since                  1.0.0
+ */
+- (instancetype)initWithFrame:(CGRect)frame
+                  andDelegate:(id<QSAutoScrollViewDelegate>)delegate
+       andScrollDirectionType:(AUTOSCROLL_DIRECTION_TYPE)directionType
+             andShowPageIndex:(BOOL)pageIndexFlag
+                 isAutoScroll:(BOOL)isAutoScroll
+                  andShowTime:(CGFloat)showTime
+               andTapCallBack:(void(^)(id params))callBack;
+
+/**
+ *  @author                 yangshengmeng, 15-03-26 17:03:14
+ *
+ *  @brief                  创建一个类广告自滚动的展示view
+ *
+ *  @param frame            大小和位置
+ *  @param delegate         代理
+ *  @param directionType    滚动方向的类型
+ *  @param pageIndexFlag    是否显示页码指示器
+ *  @param currentPage      当前页
+ *  @param isAutoScroll     是否自滚动
+ *  @param showTime         自滚动时，每一页的显示时间
+ *  @param callBack         点击当前显示页时的回调
+ *
+ *  @return                 返回当前创建的自滚动view
+ *
+ *  @since                  1.0.0
+ */
+- (instancetype)initWithFrame:(CGRect)frame
+                  andDelegate:(id<QSAutoScrollViewDelegate>)delegate
+       andScrollDirectionType:(AUTOSCROLL_DIRECTION_TYPE)directionType
+             andShowPageIndex:(BOOL)pageIndexFlag
+               andCurrentPage:(int)currentPage
+                 isAutoScroll:(BOOL)isAutoScroll
+                  andShowTime:(CGFloat)showTime
+               andTapCallBack:(void(^)(id params))callBack;
+
+/**
+ *  @author yangshengmeng, 15-03-12 17:03:15
+ *
+ *  @brief  重新加载滚动视图
+ *
+ *  @since  1.0.0
+ */
+- (void)reloadAutoScrollView;
+
+@end
--- YSMAutoScrollViewDemo/YSMAutoScrollViewDemo/ViewController.m
+++ YSMAutoScrollViewDemo/YSMAutoScrollViewDemo/ViewController.m
@@ -7,21 +7,72 @@
 //
 
 #import "ViewController.h"
+#import "QSAutoScrollView.h"
 
-@interface ViewController ()
+@interface ViewController () <QSAutoScrollViewDelegate>
+
+@property (nonatomic,retain) NSMutableArray *dataSource;//!<数据源
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    ///数据源
+    self.dataSource = [NSMutableArray arrayWithObjects:@"first.jpg",@"second.jpg",@"three.jpg", nil];
+    
+    ///尺寸
+    CGFloat deviceWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat deviceHeight = [UIScreen mainScreen].bounds.size.height;
+    
+    ///标题
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 20.0f, deviceWidth, 44.0f)];
+    titleLabel.text = @"类广告自滚动";
+    [self.view addSubview:titleLabel];
+    
+    ///加载滚动栏
+    QSAutoScrollView *scrollView = [[QSAutoScrollView alloc] initWithFrame:CGRectMake(0.0f, 64.0f, deviceWidth, deviceHeight - 64.0f) andDelegate:self andScrollDirectionType:aAutoScrollDirectionTypeRightToLeft andShowPageIndex:YES isAutoScroll:YES andShowTime:2.0f andTapCallBack:^(id params) {
+        
+        ///点击滚动页事件
+        NSLog(@"点击滚动页:%@",params);
+        
+    }];
+    [self.view addSubview:scrollView];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 滚动栏设置
+- (int)numberOfScrollPage:(QSAutoScrollView *)autoScrollView
+{
+
+    return (int)[self.dataSource count];
+
+}
+
+- (id)autoScrollViewTapCallBackParams:(QSAutoScrollView *)autoScrollView viewForShowAtIndex:(int)index
+{
+
+    return [NSString stringWithFormat:@"当前滚动页下标：%d",index];
+
+}
+
+- (UIView *)autoScrollViewShowView:(QSAutoScrollView *)autoScrollView viewForShowAtIndex:(int)index
+{
+    
+    ///获取图片
+    UIImage *tempImage = [UIImage imageNamed:self.dataSource[index]];
+
+    UIImageView *currentView = [[UIImageView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, autoScrollView.frame.size.width, autoScrollView.frame.size.width * tempImage.size.height / tempImage.size.width)];
+    currentView.image = tempImage;
+    currentView.userInteractionEnabled = YES;
+    
+    return currentView;
+
 }
 
 @end
